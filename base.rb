2dae129bbed6d711565bbefaf4a3042533af5a00
NingenUA <Ningen.UA@gmail.com>
2013-11-14 17:48:25 +0200

    connect_to_db

---
+++ base.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'active_record'
+ActiveRecord::Base.establish_connection(:adapter =>"postgresql",
+                                        :host => "78.137.1.200",
+                                        :username => "postgres",
+                                        :pass => "Gfhjkm",
+                                        :database => "pass")
+
+class Template < ActiveRecord::Base
+end
+c = Template.find(1)
+p c
